5f8cab2662a68e3a10aa2938c6cae7ea4fd9cc3b
Richard McDaniel <dannypride1@gmail.com>
2022-11-30 23:07:44 -0800

    Wrote the Kata again

--- PokerKataTests.cs
+++ PokerKataTests.cs
@@ -1,3 +1,23 @@
+// namespace C__Practice;
+
+// public class PokerKataTests
+// {
+//     [Test]
+//     public void Test_parseFace()
+//     {
+//         // arrange
+//         var cardStr = "4D";
+        
+//         // act
+//         var actualFace = PokerKata.parseFace(cardStr);
+
+//         // assert
+//         Assert.AreEqual(4, actualFace);
+
+//         Assert.AreEqual(5, PokerKata.parseFace("5D"));
+//     }
+// }
+
 namespace C__Practice;
 
 public class PokerKataTests
@@ -5,13 +25,13 @@ public class PokerKataTests
     [Test]
     public void Test_parseFace()
     {
-        // arrange
+        //arrange
         var cardStr = "4D";
-        
-        // act
+
+        //act
         var actualFace = PokerKata.parseFace(cardStr);
 
-        // assert
+        //assert
         Assert.AreEqual(4, actualFace);
 
         Assert.AreEqual(5, PokerKata.parseFace("5D"));
