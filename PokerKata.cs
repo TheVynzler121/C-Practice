7b2f1b0304c60e22138144e5e5f4d332ee8be56f
Richard McDaniel <dannypride1@gmail.com>
2022-12-01 15:56:12 -0800

    added Suit parse

--- PokerKata.cs
+++ PokerKata.cs
@@ -1,32 +1,77 @@
-// namespace C__Practice;
-
-// public class PokerKata
-// {
-// 	// faces (2-9, T, J, Q, K)
-// 	// suits // D - diamonds // S - spades // C - clubs // H - hearts
-
 //     // 4D 3H 2S TD QC - high card: queen clubs
 //     // 4D 4H 2S TD QC - pair of fours
 //     // 4D 4H 4S TD QC - three of a kind of fours
 
-
-//     public static int parseFace(string cardString)
-//     {
-//         // "4D" -> '4' -> "4" -> 4
-//         return Int32.Parse(cardString[0].ToString());
-//     }
-// }
-
 namespace C__Practice;
+
+public enum Suit {
+    Hearts,
+    Spades,
+    Diamonds,
+    Clubs
+}
+
 
 public class PokerKata
 {
     //faces (2-9, T, J, Q, K)
-    //suits // D - diamons // S - spades // C - clubs // H - hearts
+    //suits // D - diamonds // S - spades // C - clubs // H - hearts
 
-    public static int parseFace(string cardString)
+    public static Suit parseSuit(string cardString) 
     {
-        // "4D" -> '4' -> "4" -> 4
-        return Int32.Parse(cardString[0].ToString());
+        var cardSuit = cardString[1];
+        if (cardSuit == 'D')
+        {
+            return Suit.Diamonds;
+        }
+        
+        if (cardSuit == 'S')
+        {
+            return Suit.Spades;
+        }
+        
+        if (cardSuit == 'C')
+        {
+            return Suit.Clubs;
+        }
+
+        if (cardSuit == 'H')
+        {
+            return Suit.Hearts;
+        }
+    
+        throw new NotImplementedException();
+    }
+
+    public static int parseFace(string cardString) 
+    {
+        var cardFace = cardString[0];
+        if (cardFace == 'T')
+        {
+            return 10;
+        }
+
+        if (cardFace == 'J')
+        {
+            return 11;
+        }
+
+        if (cardFace == 'Q')
+        {
+            return 12;
+        }
+        
+        if (cardFace == 'K')
+        {
+            return 13;
+        }
+        
+        if (cardFace == 'A')
+        {
+            return 14;
+        }
+        
+        // else
+        return Int32.Parse(cardFace.ToString()); 
     }
 }
--- PokerKataTests.cs
+++ PokerKataTests.cs
@@ -1,29 +1,19 @@
-// namespace C__Practice;
-
-// public class PokerKataTests
-// {
-//     [Test]
-//     public void Test_parseFace()
-//     {
-//         // arrange
-//         var cardStr = "4D";
-        
-//         // act
-//         var actualFace = PokerKata.parseFace(cardStr);
-
-//         // assert
-//         Assert.AreEqual(4, actualFace);
-
-//         Assert.AreEqual(5, PokerKata.parseFace("5D"));
-//     }
-// }
-
 namespace C__Practice;
 
 public class PokerKataTests
 {
+
     [Test]
-    public void Test_parseFace()
+    public void Test_parseSuit()
+    {
+        Assert.AreEqual(Suit.Diamonds, PokerKata.parseSuit("5D"));
+        Assert.AreEqual(Suit.Spades, PokerKata.parseSuit("5S"));
+        Assert.AreEqual(Suit.Clubs, PokerKata.parseSuit("3C"));
+        Assert.AreEqual(Suit.Hearts, PokerKata.parseSuit("6H"));
+    }
+
+    [Test]
+    public void Test_parseFace_numericFace()
     {
         //arrange
         var cardStr = "4D";
@@ -35,5 +25,22 @@ public class PokerKataTests
         Assert.AreEqual(4, actualFace);
 
         Assert.AreEqual(5, PokerKata.parseFace("5D"));
+    }
+
+    [Test]
+    public void Test_parseFace_FaceCards()
+    {
+        //arrange
+        var cardStr = "TD";
+
+        //act
+        var actualFace = PokerKata.parseFace(cardStr);
+
+        //assert
+        Assert.AreEqual(10, actualFace);
+        Assert.AreEqual(11, PokerKata.parseFace("JD"));
+        Assert.AreEqual(12, PokerKata.parseFace("QD"));
+        Assert.AreEqual(13, PokerKata.parseFace("KD"));
+        Assert.AreEqual(14, PokerKata.parseFace("AD"));
     }
 }
