aa9d4379931750433d3fa3c2efb68638c0a35213
Richard McDaniel <dannypride1@gmail.com>
2022-12-23 12:59:26 -0800

    Started PokerKata over again

--- Parsers.cs
+++ Parsers.cs
@@ -6,156 +6,33 @@
 
 namespace C__Practice;
 
-public enum CardFace // This is the enum list for the card faces, which is a list of numbers with names
-{
-    Two = 2, 
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10,
-    Jack = 11,
-    Queen = 12,
-    King = 13,
-    Ace = 14,
-}
-
-public enum CardSuit
-{
-    Diamonds = 1,
-    Spades = 2,
-    Clubs = 3,
-    Hearts = 4,
-}
-
-public class WarriorSet {
-    public string name {get;set;} // "Worldbreaker"
-    public int strBonus {get;set;} 
-    public int dmgBonus {get; set;}
-}
-
-public class Player {
-    public string playerName;
-    public string PlayerClass {get;set;} // "Warrior", ...
-    public int playerLevel;
-    public int playerHp;
-    public string playerTitle;
-    public int armorClass;
-    public string playerStr;
-    public int strength;
-    public string playerDex;
-    public int dexterity;
-    public string playerCon;
-    public int consitution;
-    public const string intelligenceDisplayName = "Intelligence";
-    public int intelligence;
-}
-
-public class Helm {
-    public string name {get; set;}
-    public string rarity {get; set;}  // "Epic", "Common"
-    public string armorClass {get; set;} // "Plate", "Mail", "Cloth"
-    public int armorValue {get; set;}
-    public WarriorSet set {get; set;}  // "Worldbreaker"
-    public int levelReq {get; set;}
-    public string mainStat;
-    public int mainStatValue;
-    public string secondStat;
-    public int secStatValue;
-    public string flavorText {get; set;}
-}
-
-
-public class Card {
-
-    public CardFace face {get;set;} // public = everyone can access it. get; = everyone can get it. set; = everyone can set it.
-    public CardSuit suit {get;set;}
-}
-
 public class Parsers
 {
-
-
-    // public static List<X> dummyFunc(string cardInput) {
-        // remembered list
-
-        // foreach(var cardString in splitCards) {
-            // remembered list .Add(....)
-        // }
-
-        // return remembered list
-    // }
-
-    public static List<Card> parseCards(string cardInput)
+    //       Split       ToList         
+    // string => string[]  => List<string> 
+    public static List<string> handSplitter(string inputString)
     {
-        var cards = new List<Card>();
-
-        foreach(var cardString in cardInput.Split(' '))
+        return inputString.Split(' ').ToList();
+    }
+    //      [0]     ToString  Int32.Parse 
+    // "KD"  =>   'K' => "K"      =>      ??
+    public static int parseFace(string inputString)
+    {
+        var faceToNumb = new Dictionary<string, int>()
         {
-            cards.Add(parseCard(cardString));
+            {"T", 10},
+            {"J", 11},
+            {"Q", 12},
+            {"K", 13},
+            {"A", 14},
+        };
+        
+        var firstLetter = inputString[0].ToString();
+
+        if(faceToNumb.ContainsKey(firstLetter)) {
+            return faceToNumb[firstLetter];
+        } else {
+            return Int32.Parse(firstLetter);
         }
-        return cards;
-    }
-
-    
-
-
-    // when you "instantiate" a class, you have to call (one) if it's constructors
-    // if it has none it will call the implicit constructor + default all properties, 
-    // unless if you use the object intialization syntax
-   public static Card parseCard(string cardInput){  
-        return new Card {
-            face = extractFace(cardInput),
-            suit = extractSuit(cardInput),
-        };
-   }
-
-
-    public static Dictionary<string, int> countStrings(string[] keysToCount) {
-
-        // histogram
-        var counts = new Dictionary<string, int> { // creates the dictionary there the foreach loop will store the counted values
-            {"A", 0},
-            {"D", 0},
-            {"C", 0},
-        };
-
-        foreach(var key in keysToCount) { // Counts the keys in the array keysToCount. It will go through this loop until it reaches the end of the given array.
-            counts[key]++; // Adds 1 to the key Value in the counts Dictionary. i.e  It looks for an "A", finds the "A" in the given array, adds 1 for the value of "A" in the dictionary
-        }
-
-        return counts; // returns the number of times a Key was counted
-    }
-
-    public static CardFace extractFace(string cardString) {
-        var charToCardFace = new Dictionary<char, CardFace> { // Makes the dictionary using a char as a key and a card name from the CardFace enum as the value
-            {'2', CardFace.Two},
-            {'3', CardFace.Three},
-            {'4', CardFace.Four},
-            {'5', CardFace.Five},
-            {'6', CardFace.Six},
-            {'7', CardFace.Seven},
-            {'8', CardFace.Eight},
-            {'9', CardFace.Nine},
-            {'T', CardFace.Ten},
-            {'J', CardFace.Jack},
-            {'Q', CardFace.Queen},
-            {'K', CardFace.King},
-            {'A', CardFace.Ace},
-        }; 
-        return charToCardFace[cardString[0]]; // Searches the dictionary for the provided key and assigns the keys value to var value then Returns the value set above by var value
-    }
-
-    public static CardSuit extractSuit(string cardString){
-        var charToCardSuit = new Dictionary<char, CardSuit> {
-            {'D', CardSuit.Diamonds},
-            {'S', CardSuit.Spades},
-            {'C', CardSuit.Clubs},
-            {'H', CardSuit.Hearts},
-        };
-        return charToCardSuit[cardString[1]];
     }
 }
--- ParsersTests.cs
+++ ParsersTests.cs
@@ -1,136 +1,48 @@
-namespace C__Practice;
-
 // functions and functions calling other functions
 // tests
-// types: class, enum, int, string, char, bool, array, function
+// types: class, enum, int, string, char, bool, array, function, List
 // conditionals: if, Dictionary
 // iteratiors: foreach
 
 // array - homogenous data (all exact same type)
-// class - heterogenous data (all types allowed)
+// class - heterogenous data (all types allowed) (can hold data and/or functions)
 
-// class      Organization
-//    string  Name
-//    class   Address
-//       string Street
-//       string City
-//       string State
-//       int ZipCode
-
-//    class   MTGPlayfield
-//       Card[] Monsters
-//          string Name
-//          string Flavor
-//          string Rarity
-//          int Mana
-//       string Lands
-//       string Artifacts
-//       int Health
-
-// var aceofSpades = new Card() {
-//     face = CardFace.Ace,
-//     suit = CardSuit.Spades
-// };
-
-// var theSuit = aceofSpades.suit;
-// var theFace = aceofSpades.face;
-
-// aceOfSpades.suit = CardSuit.Hearts
-// aceOfSpades.suit == CardSuit.Hearts
-// return aceOfSpades;
-
+namespace C__Practice;
 
 public class ParsersTests
 {
 
-    [Test]
-    public void Test_CountStrings()
-    {
-        var inputStrings = new[] { "A", "A", "D", "C", "A", "C", "D", "A" };
-
-        var finalCounts = Parsers.countStrings(inputStrings);
-
-        Assert.That(finalCounts["A"], Is.EqualTo(4));
-        Assert.That(finalCounts["D"], Is.EqualTo(2));
-        Assert.That(finalCounts["C"], Is.EqualTo(2));
-    }
 
     [Test]
-    public void Test_ParseCards()
+    public void Test_handSplitter()
     {
-        var cardsString = "4D 3H 2S TD QC 5D"; // new [] { "4D", "3H", "2S", "TD", "QC" }
-        var cards = Parsers.parseCards(cardsString);
-        Assert.That(cards.Count, Is.EqualTo(6));
-        Assert.That(cards[0].face, Is.EqualTo(CardFace.Four)); 
-        Assert.That(cards[0].suit, Is.EqualTo(CardSuit.Diamonds)); 
-        Assert.That(cards[4].face, Is.EqualTo(CardFace.Queen)); 
-        Assert.That(cards[4].suit, Is.EqualTo(CardSuit.Clubs)); 
-        Assert.That(cards[5].face, Is.EqualTo(CardFace.Five)); 
-        Assert.That(cards[5].suit, Is.EqualTo(CardSuit.Diamonds)); 
+        var inputString = "4D 3H 2S TD QC";
+
+        var cardStrings = Parsers.handSplitter(inputString);
+
+        Assert.That(cardStrings, Is.EquivalentTo(new List<string>(){"4D", "3H", "2S", "TD", "QC"}));
     }
 
-    [Test]
-    public void Test_CountStrings1()
+    // "3D"  => 3
+    // "4D"  => 4
+    // "5D"  => 5
+    // "6D"  => 6
+    // "KD"  => 13
+    // "AD"  => 14
+
+    [TestCase("4D", 4)]
+    [TestCase("5D", 5)]
+    [TestCase("6D", 6)]
+    [TestCase("TD", 10)]
+    [TestCase("JD", 11)]
+    [TestCase("QD", 12)]
+    [TestCase("KD", 13)]
+    [TestCase("AD", 14)]
+    public void Test_parseFace(string inputString, int expected)
     {
-        var strings = new[] { "D", "A", "D", "C", "A", "D" }; // setting a new array/list
+        var face = Parsers.parseFace(inputString);
 
-        var counts = Parsers.countStrings(strings); // Calls the countStrings() function with the array varible strings as its parameter. then assigns the result of countStrings() as a new var called counts
-
-        Assert.That(counts["A"], Is.EqualTo(2)); // Checks if there are 2 "A"s found in the above array/list
-        Assert.That(counts["D"], Is.EqualTo(3)); // Checks if there are 3 "D"s in the above array/list
-        Assert.That(counts["C"], Is.EqualTo(1)); // Checks if there is 1 "C" in the above array/list
+        Assert.That(face, Is.EqualTo(expected));
     }
 
-    [Test]
-    public void Test_parseCard()
-    {
-        var theCard = Parsers.parseCard("2D");
-        Assert.That(theCard.face, Is.EqualTo(CardFace.Two));
-        Assert.That(theCard.suit, Is.EqualTo(CardSuit.Diamonds));
-    }
-
-    [Test]
-    public void Test_parseCard1()
-    {
-        var theCard = Parsers.parseCard("5H");
-        Assert.That(theCard.face, Is.EqualTo(CardFace.Five));
-        Assert.That(theCard.suit, Is.EqualTo(CardSuit.Hearts));
-    }
-
-    [Test]
-    public void Test_parseCard2()
-    {
-        var theCard = Parsers.parseCard("QS");
-        Assert.That(theCard.face, Is.EqualTo(CardFace.Queen));
-        Assert.That(theCard.suit, Is.EqualTo(CardSuit.Spades));
-    }
-
-    [Test]
-    public void Test_extractFace()
-    {
-        Assert.That(Parsers.extractFace("2D"), Is.EqualTo(CardFace.Two));
-        Assert.That(Parsers.extractFace("3S"), Is.EqualTo(CardFace.Three));
-        Assert.That(Parsers.extractFace("4D"), Is.EqualTo(CardFace.Four));
-        Assert.That(Parsers.extractFace("5H"), Is.EqualTo(CardFace.Five));
-        Assert.That(Parsers.extractFace("6C"), Is.EqualTo(CardFace.Six));
-        Assert.That(Parsers.extractFace("7D"), Is.EqualTo(CardFace.Seven));
-        Assert.That(Parsers.extractFace("8H"), Is.EqualTo(CardFace.Eight));
-        Assert.That(Parsers.extractFace("9C"), Is.EqualTo(CardFace.Nine));
-        Assert.That(Parsers.extractFace("TS"), Is.EqualTo(CardFace.Ten));
-        Assert.That(Parsers.extractFace("JS"), Is.EqualTo(CardFace.Jack));
-        Assert.That(Parsers.extractFace("QH"), Is.EqualTo(CardFace.Queen));
-        Assert.That(Parsers.extractFace("KC"), Is.EqualTo(CardFace.King));
-        Assert.That(Parsers.extractFace("AH"), Is.EqualTo(CardFace.Ace));
-    }
-
-
-    [Test]
-    public void Test_extractSuit()
-    {
-        Assert.That(Parsers.extractSuit("3D"), Is.EqualTo(CardSuit.Diamonds));
-        Assert.That(Parsers.extractSuit("6S"), Is.EqualTo(CardSuit.Spades));
-        Assert.That(Parsers.extractSuit("TC"), Is.EqualTo(CardSuit.Clubs));
-        Assert.That(Parsers.extractSuit("2H"), Is.EqualTo(CardSuit.Hearts));
-
-    }
 }
