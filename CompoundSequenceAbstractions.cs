345f29be33eb8b63e415992b399f6f1fb5ce78d4
Richard McDaniel <dannypride1@gmail.com>
2023-01-02 14:03:36 -0800

    CSA practice

--- CompoundSequenceAbstractions.cs
+++ CompoundSequenceAbstractions.cs
@@ -7,14 +7,56 @@ public class CompundSequenceAbstractions
 {
     public static IEnumerable<int> AddTenIfBigEnough(IEnumerable<int> numbList)
     {
-        // return numbList.Where(number => number > 3)
-        //                .Select(number => number + 10);
+        
         var outputList = new List<int>();
         foreach (var x in numbList)
         {
             if(x > 3)
             {
                 outputList.Add(x + 10);
+            }
+        }
+        return outputList;
+    }
+
+    public static string Capitalize(string x) {
+        return x[0].ToString().ToUpper() + x.Substring(1);
+    }
+
+    public static IEnumerable<string> NamesStartingWithR(IEnumerable<string> nameList)
+    {
+        
+        var outputList = new List<string>();
+        foreach (var x in nameList)
+        {
+            if(x[0] == 'r')
+            {
+                outputList.Add(Capitalize(x));
+            }
+        }
+        return outputList;
+    }
+
+    public static IEnumerable<int> ReturnNumbersGreaterThanAverage(IEnumerable<int> numbList)
+    {
+        // average add them all together and divide by the total count
+        // var numbAverage = numbList.Average();
+        // return numbList.Where(x => x > numbAverage);
+
+        var sum = 0;
+        foreach (var x in numbList)
+        {
+            sum = sum + x;
+        }
+
+        var average = sum/numbList.Count();
+
+        var outputList = new List<int>();
+        foreach (var x in numbList)
+        {
+            if(x > average)
+            {
+                outputList.Add(x);
             }
         }
         return outputList;
--- CompoundSequenceTests.cs
+++ CompoundSequenceTests.cs
@@ -3,7 +3,7 @@ namespace C__Practice;
 public class CompundSequenceTests
 {
     [Test]
-    public void Test_()
+    public void Test_AddTenIfBigEnough()
     {
         var numbers = new List<int>() {1,2,3,4,5,6};
 
@@ -15,6 +15,31 @@ public class CompundSequenceTests
         // reference equality
 
         var output = new List<int>() {14,15,16};
+
+        Assert.That(actual, Is.EquivalentTo(output));
+    }
+
+     [Test]
+    public void Test_NamesStartingWithR()
+    {
+        var names = new List<string>() {"richard", "rick", "alan", "john", "ron", "randy"};
+
+        var actual = CompundSequenceAbstractions.NamesStartingWithR(names);
+
+        var output = new List<string>() {"Richard", "Rick", "Ron", "Randy"};
+
+        Assert.That(actual, Is.EquivalentTo(output));
+    }
+
+    [Test]
+    public void Test_ReturnNumbersGreaterThanAverage()
+    {
+        var numbers = new List<int>() {1,2,3,4,5,6,7,8,9,10};
+        // 1+2+3+4+5+6+7+8+9+10/10 => 5.5
+
+        var actual = CompundSequenceAbstractions.ReturnNumbersGreaterThanAverage(numbers);
+
+        var output = new List<int>() {6,7,8,9,10};
 
         Assert.That(actual, Is.EquivalentTo(output));
     }
