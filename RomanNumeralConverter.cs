4854a55cb6dfb52e8360c23d0d1c4a70beeeeb99
Richard McDaniel <dannypride1@gmail.com>
2023-02-06 14:19:00 -0800

    Numeral to Roman practice

--- RomanNumeralConverter.cs
+++ RomanNumeralConverter.cs
@@ -12,11 +12,17 @@ public class RomanNumeralConverter {
     public static string NumberToNumeral(int remainingArabicNumbers) {
         var numerals = new Dictionary<int, string> {
             { 1000, "M" },
+            { 900, "CM"},
             { 500, "D" },
+            { 400, "CD"},
             { 100, "C" },
+            { 90, "XC"},
             { 50, "L" },
+            { 40, "XL"},         
             { 10, "X" },
+            { 9, "IX"},
             { 5, "V" },
+            { 4, "IV"},
             { 1, "I" }
         };
         var returnString = "";
--- RomanNumeralConverterTests.cs
+++ RomanNumeralConverterTests.cs
@@ -1,38 +1,18 @@
 namespace C__Practice;
 
 public class RomanNumeralConverterTests {
+    
     [TestCase(1, "I")]
     [TestCase(2, "II")]
     [TestCase(3, "III")]
-    [TestCase(4, "IIII")]
-    [TestCase(5, "V")]
-    [TestCase(6, "VI")]
-    [TestCase(7, "VII")]
-    [TestCase(8, "VIII")]
-    [TestCase(9, "VIIII")]
-    [TestCase(10, "X")]
-    [TestCase(11, "XI")]
-    [TestCase(12, "XII")]
-    [TestCase(13, "XIII")]
-    [TestCase(14, "XIIII")]
-    [TestCase(15, "XV")]
-    [TestCase(16, "XVI")]
-    [TestCase(17, "XVII")]
-    [TestCase(18, "XVIII")]
-    [TestCase(19, "XVIIII")]
-    [TestCase(20, "XX")]
-    [TestCase(30, "XXX")]
-    [TestCase(40, "XXXX")]
-    [TestCase(49, "XXXXVIIII")]
-    [TestCase(50, "L")]
-    [TestCase(51, "LI")]
-    [TestCase(55, "LV")]
-    [TestCase(99, "LXXXXVIIII")]
-    [TestCase(100, "C")]
-    [TestCase(200, "CC")]
-    [TestCase(300, "CCC")]
-    [TestCase(400, "CCCC")]
-    [TestCase(499, "CCCCLXXXXVIIII")]
+    [TestCase(4, "IV")]
+    [TestCase(9, "IX")]
+    [TestCase(19, "XIX")]
+    [TestCase(49, "XLIX")]
+    [TestCase(99, "XCIX")]
+    [TestCase(499, "CDXCIX")]
+    [TestCase(999, "CMXCIX")]
+    
     public void Test_NumberToNumeral(int input, string output) {
         var actual = RomanNumeralConverter.NumberToNumeral(input);
 
