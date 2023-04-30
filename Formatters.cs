43cc14dd91e2f05f91523cd7c74cfc03266c2a6a
Richard McDaniel <dannypride1@gmail.com>
2023-04-30 13:33:12 -0700

    C# notes

--- Formatters.cs
+++ Formatters.cs
@@ -7,15 +7,15 @@ public class Formatters {
     //           formatSuit/formatFace
     //  Card                 ->      string
     public static string printCard(Card card) {
-        var cardSuit = formatSuit(card.Suit);
-        var cardFace = formatFace(card.Face);
-        var formatedCard = $"{cardFace} {cardSuit}";
+        var cardSuit = formatSuit(card.Suit); //passes the Card's suit to formatSuit
+        var cardFace = formatFace(card.Face); //passes the Card's face to formatFace
+        var formatedCard = $"{cardFace} {cardSuit}"; //$"" makes a string //makes a string with the face and suit
 
-        return formatedCard;
+        return formatedCard; // returns the string of face and suit
     }
 
-    public static string formatFace(int inputInt) {
-        var x = new Dictionary<int, string> {
+    public static string formatFace(int inputInt) { //takes an Int and returns a string
+        var x = new Dictionary<int, string> { // a collection of {keys, values}
             { 2, "Two" },
             { 3, "Three" },
             { 4, "Four" },
@@ -31,12 +31,12 @@ public class Formatters {
             { 14, "Ace" }
         };
 
-        return x[inputInt];
+        return x[inputInt]; //return the value that matches the input int
     }
 
-    public static string formatSuit((bool, bool) suitValue) {
-        if (suitValue == (true, true)) {
-            return "Hearts";
+    public static string formatSuit((bool, bool) suitValue) { //takes a touple of (bool, bool) and returns a string
+        if (suitValue == (true, true)) { //if the touple is true, true
+            return "Hearts"; // early out return "hearts"
         }
 
         if (suitValue == (true, false)) {
--- Parsers.cs
+++ Parsers.cs
@@ -18,41 +18,41 @@
 
 namespace C__Practice;
 
-public class Card {
-    public int Face { get; set; }
-    public (bool, bool) Suit { get; set; }
+public class Card { 
+    public int Face { get; set; } // an int representing the card Face
+    public (bool, bool) Suit { get; set; }// a touple of (bool, bool) representing the card Suit
 }
 
 public class Parsers {
-    // "3D"
-    public static Card parseCard(string cardString) {
+
+    public static Card parseCard(string cardString) { //takes a string and returns a parsed Card class
         var x = new Card {
-            Face = parseFace(cardString),
-            Suit = parseSuit(cardString)
+            Face = parseFace(cardString), //call parseFace to parse the card Face
+            Suit = parseSuit(cardString) //call parseSuit to parse the card Suit
         };
-        return x;
+        return x; //return the parsed Card
     }
 
-    public static IEnumerable<Card> parseHand(string handString) {
+    public static IEnumerable<Card> parseHand(string handString) { //takes a string and return a List of Card
         Thread.Sleep(1000);
-        return handSplitter(handString).Select(parseCard);
+        return handSplitter(handString).Select(parseCard);//split the hand into a list of parsed cards 
     }
 
 
-    public static (bool, bool) parseSuit(string card) {
-        if (card[1] == 'H') {
+    public static (bool, bool) parseSuit(string card) { //takes a string and returns a touple of (bool, bool)
+        if (card[1] == 'H') { //if the cards second position is an H, early out return true,true
             return (true, true);
         }
 
-        if (card[1] == 'S') {
+        if (card[1] == 'S') { //if the cards second position is an S, early out return true, false
             return (true, false);
         }
 
-        if (card[1] == 'C') {
+        if (card[1] == 'C') { //if the cards second position is a C, early out return false, true
             return (false, true);
         }
 
-        if (card[1] == 'D') {
+        if (card[1] == 'D') { //if the cards second position is a D, early out return false, false
             return (false, false);
         }
 
@@ -61,14 +61,14 @@ public class Parsers {
 
     //       Split       ToList         
     // string => string[]  => List<string> 
-    public static List<string> handSplitter(string inputString) {
-        return inputString.Split(' ').ToList();
+    public static List<string> handSplitter(string inputString) { //takes a string and returns a list of strings
+        return inputString.Split(' ').ToList(); //make a list of the input split by spaces
     }
 
     //      [0]     ToString  Int32.Parse 
     // "KD"  =>   'K' => "K"      =>      ??
-    public static int parseFace(string inputString) {
-        var faceToNumb = new Dictionary<string, int> {
+    public static int parseFace(string inputString) { //takes a string and returns an int
+        var faceToNumb = new Dictionary<string, int> { //collection of {keys, values}
             { "T", 10 },
             { "J", 11 },
             { "Q", 12 },
@@ -76,12 +76,12 @@ public class Parsers {
             { "A", 14 }
         };
 
-        var faceString = inputString[0].ToString();
+        var faceString = inputString[0].ToString(); //take the first position of input and turn it into a string
 
-        if (faceToNumb.ContainsKey(faceString)) {
-            return faceToNumb[faceString];
+        if (faceToNumb.ContainsKey(faceString)) { //if the dictionary faceToNumb has a key matching faceString
+            return faceToNumb[faceString]; //return the value of the faceToNumb Key that matches faceString
         }
 
-        return int.Parse(faceString);
+        return int.Parse(faceString); //parse the faceString into an int and return it
     }
 }
